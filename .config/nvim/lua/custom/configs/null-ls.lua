7d1b3b84d0d19516b14c4fddd0126f83c37da795
ayoub <ayoubousbia02@gmail.com>
2024-08-29 00:00:26 +0100

    remove ruff, eslint_lsp for debug

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -7,9 +7,7 @@ local opts = {
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.clang_format,
-    null_ls.builtins.diagnostics.ruff,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettierd,
   },
   on_attach = function(client, bufnr)
